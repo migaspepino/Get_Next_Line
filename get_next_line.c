cf665f67adddeac17d6e87e7342244473e9ac3c4
Miguel <miguel@Miguels-Air.lan>
2021-12-12 22:54:08 +0000

    1st commit

---
+++ get_next_line.c
@@ -0,0 +1,76 @@
+#include "get_next_line.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = -1;
+	if ((dst != src) && n)
+		while (++i < n)
+			((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+	return (dst);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*output(char **backup, char *hBackup, int ret, int fd)
+{
+	int		value;
+	char	*temp;
+
+	if (ret < 0)
+		return (NULL);
+	else if (ret == 0 && backup[fd] == NULL)
+		return (NULL);
+	else
+	{
+		value = (int)(ft_strchr(backup[fd], '\n') - backup[fd] + 1);
+		hBackup = ft_substr(backup[fd], 0, value);
+		temp = ft_substr(backup[fd], value, BUFFER_SIZE);
+		free(backup[fd]);
+		if (temp[0] == '\0')
+		{
+			free(temp);
+			temp = NULL;
+		}
+		backup[fd] = temp;
+		return (hBackup);
+	}
+}
+
+char	*get_next_line(int fd)
+{
+	int			ret;
+	char 		buf[BUFFER_SIZE + 1];
+	static char	*backup[NUM_OF_FD];
+	char 		*hBackup;
+
+	if (fd < 0)
+		return (NULL);
+	while ((ret = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+		buf[BUFFER_SIZE] = '\0';
+		if (backup[fd] == NULL)
+			backup[fd] = ft_strdup(buf);
+		else
+		{
+			hBackup = ft_strjoin(backup[fd],buf);
+			free(backup[fd]);
+			backup[fd] = hBackup;
+			ft_memset(buf, 0, BUFFER_SIZE);
+		}
+		if (ft_strchr(backup[fd],'\n'))
+			break;
+	}
+	return (output(backup, hBackup, ret, fd));
+}
